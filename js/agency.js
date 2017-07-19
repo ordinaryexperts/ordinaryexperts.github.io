361af587ddeb948033d8a4039aa7f7d3fb1cc8d7
Dylan Vaughn <dylancvaughn@gmail.com>
2017-07-19 16:07:50 -0700

    changing line endings

--- js/agency.js
+++ js/agency.js
@@ -5,43 +5,51 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
+function scrollTo(anchor) {
+  $('html, body').stop().animate({
+    scrollTop: $(anchor).offset().top
+  }, 1500, 'easeInOutExpo');
+}
+
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('a.page-scroll').bind('click', function(event) {
+    var href = $(this).attr('href');
+    scrollTo(href);
+    history.pushState(null, null, href);
+    event.preventDefault();
+  });
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+  target: '.navbar-fixed-top'
 })
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+  $('.navbar-toggle:visible').click();
 });
 
 // http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
 function GetURLParameter(sParam)
 {
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++)
+  {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam)
     {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
+      return sParameterName[1];
     }
+  }
 }
 
 $(function() {
   if (GetURLParameter('contacted') == '1') {
     $('#contacted-thanks').show();
+  }
+  if (window.location.hash) {
+    scrollTo(window.location.hash);
   }
 });
